fcb97d886eb3742e1a9a765a0e79569c33097c19
Norm Santos <santnorm@gmail.com>
2022-07-25 21:34:22 -0600

    WIP

--- index.mjs
+++ index.mjs
@@ -48,21 +48,28 @@ const gitRefToEnv = async(gitRef, token) => {
 const runQuery = async(query, token) => {
 	const config = {
 		headers: {
-			'Content-Type': 'application/json',
-			Accept: 'application/json',
-			"authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiZjIxMDFiYTUtOThlMS00YjQ5LWEzNWYtOGU5ZjUxOTMwY2UzIiwidGVuYW50X2lkIjoiN2M2NjY5OGUtZDZlYi00ODEyLTk3YmYtZTJjYzkzMWFmYTRiIiwiaWF0IjoxNjU4ODAxNDYzLCJleHAiOjE2NTg4MDIwNjMsImp0aSI6ImIyZTM1NDFhLWZkY2MtNDdlOS1iMTU3LWM3OTMxYjNjZjQwNiIsImlzcyI6IlByZWZlY3QgQ2xvdWQiLCJhdWQiOiJQcmVmZWN0IENsb3VkIEFQSSJ9.RCos-sRChtbEDZEnZfmjPCiTcieFvW4dg1n671o8kJN4OzLF9WJ_cmA1Ajqzy3DGPzBCfTxFd7Cg_0dh5IJOrsqddtV4GkTsQQxaYCqov_Lqqr9kHU-hsh-MgkzuFayYq7zCgq3xgGSEnKavafwAu8x458qUw34J1z0BlTVIK-gybTiuHEUXudtMM_Let9BMX90E6rzNOZSoxOkM8jXwc0fDtQ-Fc22ddikI5DTtxbrHThhDo_JhQkvexRgsR--B1jh09OJmP1YHtJ8HXZjbYYVg_ZNj3c9f93o5JZpd1BPUKBS9uQnfcq6Lqu_uojYlIJxZHgRNCH5CZi-MRW1keQ"
+			'content-type': 'application/json',
+			"authorization": `Bearer ${token}`,
+			'x-prefect-interactive-api': false,
 		},
+	};
+
+	const data =  {
 		query
 	};
 
 	const url = 'https://api.prefect.io/graphql';
 
-	const [errA, response] = await to(axios.get(url, config));
+	const [errA, response] = await to(axios.post(url, data, config));
 	if (errA) {
 		throw new Error(errA);
 	}
-console.log(response);
-	return _.get(response, 'data.value.computed', '');
+	const errors = _.get(response, 'data.errors');
+	if(errors !== undefined) {
+		throw new Error(errors[0].message);
+	}
+
+	return _.get(response, 'data.data', '');
 }
 
 const run = async() => {
@@ -76,7 +83,7 @@ const run = async() => {
 		throw new Error('action is not set');
 	}
 
-	if(action !== 'start' || action !== 'stop') {
+	if(action !== 'start' &&  action !== 'stop') {
 		throw new Error('action must be either `start` or `stop`');
 
 	}
@@ -90,19 +97,18 @@ const run = async() => {
 		throw new Error(`${branch} is not a valid branch`);
 	}
 
-const query = `
-{
+const query = `{
 	flow {
 		id
 		name
 	}
-}
-`;
+}`;
 
-	const [errA] = await to(runQuery(query, prefect_account_key));
+	const [errA, data] = await to(runQuery(query, prefect_account_key));
 	if (errA) {
 		throw new Error(errA);
 	}
+console.log(data);
 
 //	core.setOutput("doppler_config", doppler_config);
 //	core.exportVariable('DOPPLER_CONFIG', doppler_config);
